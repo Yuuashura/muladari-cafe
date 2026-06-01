c85118bdef76da444695208f223d2e3828ae0fe6
YuuAshura <tzyudistira@gmail.com>
2026-06-01 20:42:54 +0700

    feat: expand gallery data list to all 14 vintage items with custom titles and captions

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,38 +1,86 @@
 export const galleryList = [
   {
     id: 1,
-    title: 'Identitas Muladari Kopi',
-    caption: 'Logo resmi Muladari Kopi yang melambangkan kebersamaan dan cita-cita mulia.',
-    image: '/images/instagram_post1.jpg'
+    title: 'Koper Besi Era Kolonial',
+    caption: 'Koper besi antik asal zaman penjajahan Belanda dengan nuansa rustik yang bernilai sejarah tinggi',
+    image: '/images/Galery/1.jpg'
   },
   {
     id: 2,
-    title: 'Sentuhan Latte Art',
-    caption: 'Kombinasi keindahan visual dan kelembutan rasa di setiap tegukan Cappuccino.',
-    image: 'https://images.unsplash.com/photo-1541167760496-1628856ab772?q=80&w=600&auto=format&fit=crop'
+    title: 'TV Jadul Era 70s',
+    caption: 'Televisi kayu antik bergaya retro atau dikenal sebagai TV Jengki yang populer di era 1970-an',
+    image: '/images/Galery/2.jpg'
   },
   {
     id: 3,
-    title: 'Diagram "Lagi Stres"',
-    caption: 'Humor dan sains di balik kenikmatan ngopi yang terbukti meredakan stres harian Anda.',
-    image: '/images/instagram_post2.jpg'
+    title: 'Nostalgia Elektronik 70-90s',
+    caption: 'Koleksi barang elektronik lawas mulai dari TV tabung antik, radio tape boombox, telepon kabel, hingga kamera SLR analog klasik',
+    image: '/images/Galery/3.jpg'
   },
   {
     id: 4,
-    title: 'Sudut Nyaman Muladari',
-    caption: 'Detail dekorasi bernuansa kayu yang menghadirkan suasana hangat dan nostalgik.',
-    image: 'https://images.unsplash.com/photo-1453614512568-c4024d13c247?q=80&w=600&auto=format&fit=crop'
+    title: 'Cermin Jadul Ukir',
+    caption: 'Cermin dinding dengan bingkai kayu solid berukir detail khas era 80-an yang menghadirkan keindahan vintage',
+    image: '/images/Galery/4.jpg'
   },
   {
     id: 5,
-    title: 'Nilai dan Semangat',
-    caption: 'Komitmen kami menyajikan racikan kopi dengan ketulusan dan semangat kebersamaan.',
-    image: '/images/instagram_post3.jpg'
+    title: 'Sepeda Federal',
+    caption: 'Sepeda gunung Federal klasik yang populer di Indonesia pada era 1980-an hingga 1990-an',
+    image: '/images/Galery/5.jpg'
   },
   {
     id: 6,
-    title: 'Keseruan Komunitas',
-    caption: 'Momen hangat dan santai bersama rekan sejawat di area outdoor Muladari Coffee.',
-    image: '/images/instagram_post4.jpg'
+    title: 'Pigura Vintage',
+    caption: 'Lukisan oval potret wanita dengan busana era 1960-an dalam bingkai ukiran gaya Baroque yang mewah',
+    image: '/images/Galery/6.jpg'
+  },
+  {
+    id: 7,
+    title: 'Topeng Asmat Papua',
+    caption: 'Topeng kayu tradisional bercorak etnik eksotis dengan teknik lukis titik (dot painting) khas Papua',
+    image: '/images/Galery/7.jpg'
+  },
+  {
+    id: 8,
+    title: 'Patung Dewi Sri',
+    caption: 'Ukiran kayu tradisional Bali menggambarkan sosok Dewi Sri sebagai simbol kemakmuran dan kesuburan',
+    image: '/images/Galery/8.jpg'
+  },
+  {
+    id: 9,
+    title: 'Patung Seni Tradisional',
+    caption: 'Koleksi patung kayu eksotis mulai dari patung primitif suku Kamoro Papua hingga patung penari Legong khas Bali',
+    image: '/images/Galery/9.jpg'
+  },
+  {
+    id: 10,
+    title: 'Figura Seni Bali',
+    caption: 'Lukisan tradisional Bali yang menampilkan sosok Dewi Saraswati dan penari Bali klasik dalam bingkai ukir kayu estetis',
+    image: '/images/Galery/10.jpg'
+  },
+  {
+    id: 11,
+    title: 'Uang Kertas Kuno',
+    caption: 'Koleksi uang kertas kuno Indonesia (seri tahun 1950-an) yang dibingkai rapi bernilai numismatik tinggi',
+    image: '/images/Galery/11.jpg'
+  },
+  {
+    id: 12,
+    title: 'Keris dan Kujang',
+    caption: 'Senjata tradisional khas Nusantara berupa Keris Melayu/Minang dan Kujang Sunda sebagai pajangan antik bernilai seni',
+    image: '/images/Galery/12.jpg'
+  },
+  {
+    id: 13,
+    title: 'Patung Garuda Wisnu Kencana',
+    caption: 'Karya seni ukir kayu khas Bali menggambarkan Dewa Wisnu yang bertengger gagah di atas burung mitologi Garuda',
+    image: '/images/Galery/13.jpg'
+  },
+  {
+    id: 14,
+    title: 'Patung Dewi Sita',
+    caption: 'Ukiran kayu vertikal khas Bali yang menggambarkan sosok Dewi Sita dengan warna polikrom antik bernuansa rustic',
+    image: '/images/Galery/14.jpg'
   }
 ];
