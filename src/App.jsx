790aa26ed2319fdb9bcf5c846b670ccfbb8518df
YuuAshura <tzyudistira@gmail.com>
2026-06-02 11:44:58 +0700

    style: scale down background coffee beans sizes to be small and elegant

--- src/App.jsx
+++ src/App.jsx
@@ -47,45 +47,45 @@ const FloatingCoffeeBean = ({ top, left, right, size, speed, rot, color }) => {
 
 const floatingBeansData = [
   // Section 1: Hero / Top (0% - 15%)
-  { top: '3%', left: '4%', size: 'w-10 h-10 md:w-14 md:h-14', speed: -180, rot: 45, color: 'text-caramel/9' },
-  { top: '7%', right: '5%', size: 'w-16 h-16 md:w-20 md:h-20', speed: 220, rot: -60, color: 'text-espresso/8' },
-  { top: '11%', left: '80%', size: 'w-8 h-8 md:w-12 md:h-12', speed: -140, rot: 90, color: 'text-caramel/10' },
-  { top: '14%', left: '6%', size: 'w-12 h-12 md:w-16 md:h-16', speed: 170, rot: -15, color: 'text-espresso/7' },
+  { top: '3%', left: '4%', size: 'w-5 h-5 md:w-7 md:h-7', speed: -180, rot: 45, color: 'text-caramel/9' },
+  { top: '7%', right: '5%', size: 'w-8 h-8 md:w-10 md:h-10', speed: 220, rot: -60, color: 'text-espresso/8' },
+  { top: '11%', left: '80%', size: 'w-4 h-4 md:w-6 md:h-6', speed: -140, rot: 90, color: 'text-caramel/10' },
+  { top: '14%', left: '6%', size: 'w-6 h-6 md:w-8 md:h-8', speed: 170, rot: -15, color: 'text-espresso/7' },
 
   // Section 2: Story (15% - 30%)
-  { top: '19%', left: '10%', size: 'w-20 h-20 md:w-24 md:h-24', speed: 300, rot: 120, color: 'text-espresso/6' },
-  { top: '23%', right: '8%', size: 'w-10 h-10 md:w-14 md:h-14', speed: -250, rot: -30, color: 'text-caramel/9' },
-  { top: '26%', left: '75%', size: 'w-14 h-14 md:w-18 md:h-18', speed: 200, rot: 75, color: 'text-espresso/8' },
-  { top: '29%', left: '3%', size: 'w-12 h-12 md:w-16 md:h-16', speed: -190, rot: -45, color: 'text-caramel/10' },
+  { top: '19%', left: '10%', size: 'w-10 h-10 md:w-12 md:h-12', speed: 300, rot: 120, color: 'text-espresso/6' },
+  { top: '23%', right: '8%', size: 'w-5 h-5 md:w-7 md:h-7', speed: -250, rot: -30, color: 'text-caramel/9' },
+  { top: '26%', left: '75%', size: 'w-7 h-7 md:w-9 md:h-9', speed: 200, rot: 75, color: 'text-espresso/8' },
+  { top: '29%', left: '3%', size: 'w-6 h-6 md:w-8 md:h-8', speed: -190, rot: -45, color: 'text-caramel/10' },
 
   // Section 3: Menu (30% - 50%)
-  { top: '34%', right: '4%', size: 'w-22 h-22 md:w-26 md:h-26', speed: 350, rot: 160, color: 'text-espresso/6' },
-  { top: '38%', left: '12%', size: 'w-8 h-8 md:w-12 md:h-12', speed: -160, rot: -90, color: 'text-caramel/11' },
-  { top: '42%', right: '10%', size: 'w-18 h-18 md:w-22 md:h-22', speed: 260, rot: 110, color: 'text-espresso/7' },
-  { top: '45%', left: '5%', size: 'w-16 h-16 md:w-20 md:h-20', speed: -280, rot: -75, color: 'text-caramel/9' },
-  { top: '49%', right: '7%', size: 'w-10 h-10 md:w-14 md:h-14', speed: 190, rot: 45, color: 'text-espresso/8' },
+  { top: '34%', right: '4%', size: 'w-11 h-11 md:w-13 md:h-13', speed: 350, rot: 160, color: 'text-espresso/6' },
+  { top: '38%', left: '12%', size: 'w-4 h-4 md:w-6 md:h-6', speed: -160, rot: -90, color: 'text-caramel/11' },
+  { top: '42%', right: '10%', size: 'w-9 h-9 md:w-11 md:h-11', speed: 260, rot: 110, color: 'text-espresso/7' },
+  { top: '45%', left: '5%', size: 'w-8 h-8 md:w-10 md:h-10', speed: -280, rot: -75, color: 'text-caramel/9' },
+  { top: '49%', right: '7%', size: 'w-5 h-5 md:w-7 md:h-7', speed: 190, rot: 45, color: 'text-espresso/8' },
 
   // Section 4: Vibes (50% - 65%)
-  { top: '53%', right: '3%', size: 'w-12 h-12 md:w-16 md:h-16', speed: 180, rot: 40, color: 'text-espresso/8' },
-  { top: '57%', left: '8%', size: 'w-20 h-20 md:w-24 md:h-24', speed: -320, rot: -120, color: 'text-caramel/6' },
-  { top: '61%', right: '15%', size: 'w-10 h-10 md:w-14 md:h-14', speed: 150, rot: 80, color: 'text-espresso/9' },
-  { top: '64%', left: '4%', size: 'w-14 h-14 md:w-18 md:h-18', speed: -210, rot: -65, color: 'text-caramel/10' },
+  { top: '53%', right: '3%', size: 'w-6 h-6 md:w-8 md:h-8', speed: 180, rot: 40, color: 'text-espresso/8' },
+  { top: '57%', left: '8%', size: 'w-10 h-10 md:w-12 md:h-12', speed: -320, rot: -120, color: 'text-caramel/6' },
+  { top: '61%', right: '15%', size: 'w-5 h-5 md:w-7 md:h-7', speed: 150, rot: 80, color: 'text-espresso/9' },
+  { top: '64%', left: '4%', size: 'w-7 h-7 md:w-9 md:h-9', speed: -210, rot: -65, color: 'text-caramel/10' },
 
   // Section 5: Gallery (65% - 80%)
-  { top: '68%', left: '3%', size: 'w-16 h-16 md:w-20 md:h-20', speed: -220, rot: -50, color: 'text-caramel/8' },
-  { top: '72%', right: '6%', size: 'w-22 h-22 md:w-26 md:h-26', speed: 380, rot: 135, color: 'text-espresso/5' },
-  { top: '76%', left: '80%', size: 'w-12 h-12 md:w-16 md:h-16', speed: -170, rot: 60, color: 'text-caramel/10' },
-  { top: '79%', left: '6%', size: 'w-8 h-8 md:w-12 md:h-12', speed: 190, rot: -80, color: 'text-espresso/11' },
+  { top: '68%', left: '3%', size: 'w-8 h-8 md:w-10 md:h-10', speed: -220, rot: -50, color: 'text-caramel/8' },
+  { top: '72%', right: '6%', size: 'w-11 h-11 md:w-13 md:h-13', speed: 380, rot: 135, color: 'text-espresso/5' },
+  { top: '76%', left: '80%', size: 'w-6 h-6 md:w-8 md:h-8', speed: -170, rot: 60, color: 'text-caramel/10' },
+  { top: '79%', left: '6%', size: 'w-4 h-4 md:w-6 md:h-6', speed: 190, rot: -80, color: 'text-espresso/11' },
 
   // Section 6: Events (80% - 90%)
-  { top: '83%', right: '4%', size: 'w-18 h-18 md:w-22 md:h-22', speed: -300, rot: 150, color: 'text-caramel/7' },
-  { top: '86%', left: '10%', size: 'w-14 h-14 md:w-18 md:h-18', speed: 240, rot: -35, color: 'text-espresso/8' },
-  { top: '89%', right: '8%', size: 'w-10 h-10 md:w-14 md:h-14', speed: -150, rot: 110, color: 'text-caramel/9' },
+  { top: '83%', right: '4%', size: 'w-9 h-9 md:w-11 md:h-11', speed: -300, rot: 150, color: 'text-caramel/7' },
+  { top: '86%', left: '10%', size: 'w-7 h-7 md:w-9 md:h-9', speed: 240, rot: -35, color: 'text-espresso/8' },
+  { top: '89%', right: '8%', size: 'w-5 h-5 md:w-7 md:h-7', speed: -150, rot: 110, color: 'text-caramel/9' },
 
   // Section 7: Location / Contact (90% - 100%)
-  { top: '92%', right: '10%', size: 'w-12 h-12 md:w-16 md:h-16', speed: -180, rot: 100, color: 'text-caramel/9' },
-  { top: '95%', left: '3%', size: 'w-18 h-18 md:w-22 md:h-22', speed: 210, rot: -110, color: 'text-espresso/8' },
-  { top: '98%', right: '5%', size: 'w-10 h-10 md:w-14 md:h-14', speed: -130, rot: 25, color: 'text-caramel/10' }
+  { top: '92%', right: '10%', size: 'w-6 h-6 md:w-8 md:h-8', speed: -180, rot: 100, color: 'text-caramel/9' },
+  { top: '95%', left: '3%', size: 'w-9 h-9 md:w-11 md:h-11', speed: 210, rot: -110, color: 'text-espresso/8' },
+  { top: '98%', right: '5%', size: 'w-5 h-5 md:w-7 md:h-7', speed: -130, rot: 25, color: 'text-caramel/10' }
 ];
 
 const MainLayout = () => {
