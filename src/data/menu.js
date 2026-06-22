f0404a069262e988a3fd7d4d4c9c17c1d043f7e2
YuuAshura <tzyudistira@gmail.com>
2026-06-23 00:34:55 +0700

    feat: add menu data structure and initial menu items including promotional image

--- src/data/menu.js
+++ src/data/menu.js
@@ -13,5 +13,12 @@ export const menuItems = [
     category: 'all',
     description: 'List Menu Muladari Kopi Sementara',
     image: '/images/New folder/WhatsApp Image 2026-06-07 at 13.51.38.jpeg'
+  },
+    {
+    id: 2,
+    name: 'HOT NEWS',
+    category: 'all',
+    description: 'Sekarang Muladari Kopi sudah tersedia di MAXIM',
+    image: '/images/Menu/2.jpeg'
   }
 ];
